671473266dc3eda1e3fc205596d618a4ba69dac0
Rohit Singh <rsrohitsingh682@gmail.com>
2024-07-10 14:50:12 +0530

    use valid derrivers

--- src/nix/nix_store.rs
+++ src/nix/nix_store.rs
@@ -92,7 +92,7 @@ impl NixStoreCmd {
     /// Return the derivation used to build the given build output.
     async fn nix_store_query_deriver(&self, out_path: PathBuf) -> Result<DrvOut, NixCmdError> {
         let mut cmd = self.command();
-        cmd.args(["--query", "--deriver", &out_path.to_string_lossy().as_ref()]);
+        cmd.args(["--query", "--valid-derivers", &out_path.to_string_lossy().as_ref()]);
         nix_rs::command::trace_cmd(&cmd);
         let out = cmd
             .output()
